5c29785627ce711ef81a47f81b971d7661a29e41
laicansat <laicansat2@gmail.com>
2015-09-12 01:21:26 -0300

    SDcard library

--- lib/laicansat/examples/ThermometerTest/ThermometerTest.ino
+++ lib/laicansat/examples/ThermometerTest/ThermometerTest.ino
@@ -8,15 +8,16 @@
  BMP085_THERMOMODE  =   5
  MEAN_THERMOMODE    =   6
 **/
-
+const int led = LED_BUILTIN;
 void setup()
 {
   Serial.begin(9600);
   
   Serial.println("Teste");
-  laicansat.thermo->begin(BMP180_THERMOMODE); 
-  laicansat.bar->begin(BMP180_BAROMODE);
+  laicansat.thermo->begin(BMP085_THERMOMODE); 
+  laicansat.bar->begin(BMP085_BAROMODE);
   Serial.println("Initiated!");
+   pinMode(led, OUTPUT);
   delay(100);
 }
 
@@ -27,5 +28,8 @@ void loop()
   
   Serial.println(temperature);
   Serial.println(pressure);
+   digitalWrite(led, HIGH);
+  delay(100);
+  digitalWrite(led, LOW);
   delay(100);
 }
---
+++ lib/laicansat/utility/Memory.h
@@ -0,0 +1,19 @@
+#ifndef _MEMORY_H
+#define _MEMORY_H
+
+#include "Arduino.h"
+#include "SD.h"
+
+#include "../laicansat.h"
+
+class MemoryClass{
+
+public:
+	void inicializeSDcard();
+	void 
+private:
+	const int chipSelect = 62;
+}
+
+
+#endif
